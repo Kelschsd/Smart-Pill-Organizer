9c1468434b055ea9de4a1c12a26e70a07bc9c863
Samuel Kelsch <123428419+Kelschsd@users.noreply.github.com>
2023-05-02 16:24:56 -0400

    Update SmartOrganizer.ino

--- SmartOrganizer/SmartOrganizer.ino
+++ SmartOrganizer/SmartOrganizer.ino
@@ -28,15 +28,12 @@ void set_alarm(); 	// Used to set alarm time
 
 bool alarms_on = 1;	// Change to 0 to disable alarms
 
-struct alarms
-{
-	int weekDay[13];  // (0-6) (Sun - Sat)
-	int hour[13];     // (0-23)
-	int min[13];      // (0-59)
-	bool alarm_on[13];    // (0-1)
-};
-
-alarms alarm = {};
+// List of all alarms and their values. 14 total possible alarms, where the index
+// of the value in the array represents the alarm number.
+uint8_t weekDay[14] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};    // (0-6) (Sun - Sat)
+uint8_t hour[14]    = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};    // (0-23)
+uint8_t min[14]     = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};    // (0-59)
+bool alarm_on[14]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};    // (0-1)
 
 /*
 	Setup method to initialize components
@@ -58,12 +55,13 @@ void setup(void) {
 /*
   Method to check if current time is the same as an alarm time that is turned ON
 */
-bool isTimeAlarm(DateTime now) {
+bool isTimeAlarm() {
+  DateTime now = RTC.now(); 
   for (int i=0; i<14; i++) {
-    if (alarm.alarm_on[i] == 1) { // only check time against set alarm time if alarm is ON (1)
-     if (alarm.weekDay[i] == now.dayOfWeek()) { 
-      if (alarm.hour[i] == now.hour()) { 
-        if (alarm.min[i] == now.minute()) { 
+    if (alarm_on[i] == 1) { // only check time against set alarm time if alarm is ON (1)
+     if ((weekDay[i]) == (now.dayOfWeek())) {        
+      if (hour[i] == now.hour()) { 
+        if (min[i] == now.minute()) { 
           return true;
         }
        }
@@ -94,27 +92,22 @@ void printTime () {
     Serial.println(); 
 }
 
-/*
-	Main method that loops forever
-*/
-void loop() {
-/* Test Code  
-  Serial.println("DEBUG 1");
+void loop() { 
   DateTime now = RTC.now(); 
-  Serial.println("DEBUG 2");
-  alarm.alarm_on[0] = 1;  
-  alarm.weekDay[0] = 1;
-  alarm.hour[0] = 2;
-  alarm.min[0] = 30;
-  Serial.println("DEBUG 3");
-  */
-  Serial.println("Is alarm time?: "+ isTimeAlarm(now));
+
+// Alarm hardcoded for testing
+  alarm_on[0] = 1;  
+  weekDay[0] = 2;
+  hour[0] = 16;
+  min[0] = 19;
+
   printTime();
-  if (isTimeAlarm(now) == true){
-    // notify();      
+
+  if ((isTimeAlarm() == true) && (alarms_on == 1)){
+    //notify();     
+  } else {
+    delay(1000);
   }
-  // If time == alarm && alarm_on
-    //notify();
   // if keypad_flag == 1
     // if key == "set alarm key"
       // set_alarm();
