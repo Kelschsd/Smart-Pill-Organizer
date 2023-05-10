94f3ebb15c59c8d101959736bb9ae7c8506224ee
Samuel Kelsch <123428419+Kelschsd@users.noreply.github.com>
2023-05-10 16:03:16 -0400

    Add files via upload

--- SmartOrganizer/SmartOrganizer.ino
+++ SmartOrganizer/SmartOrganizer.ino
@@ -5,51 +5,40 @@
 */
 
 
-#include<NoDelay.h>
-#include<LiquidCrystal.h>
+#include <NoDelay.h>
+#include <LiquidCrystal.h>
 #include <Wire.h>
-#include "RTClib.h"
+#include "src/libs/RTClib.h"
 RTC_DS1307 RTC;
-
-/*
-	Protoype Functions
-*/
-void notify();	  	// Main function that implements all notifications
-void get_time();  	// Reads time from RTC and stores readings to global varriables
-void print_time();	// Prints a time to LCD
-void light_LED(); 	// Writes LED address to registers to light, will keep LEDs on if not answered
-void send_SMS();  	// Send SMS from notify()
-void get_key();	  	// Read value on analog pin and translate to what button pushed
-void set_time();  	// Used to set time
-void set_alarm(); 	// Used to set alarm time
 
 /*
 	Project varriables
 */
 bool alarms_on = 1;	// Change to 0 to disable alarms
-
 // Create an LCD object. Parameters: (RS, E, Data Lines)
-LiquidCrystal lcd = LiquidCrystal(2,3, 4,5,6,7);
+LiquidCrystal lcd = LiquidCrystal(2,3,4,5,6,7);
 
 int stClk = 8;  // RCLK (12) primary register
 int srClk = 12;  // SRCLK (11) primary register
 int dataPin = 11;  // SER (14) primary register
 int dataPin2 = 10;  // SER (14) secondary register
+
+///////////////////////////////////////////////////Remove PB?
 int button1 = 9;  // push button
 int button1State = 0;
+////////////////////////////////////////////////////
 
 char output[100];
 int LED = 0;  // Which LED should be illuminated
-bool hasNotification = 0;
 
 // List of all alarms and their values. 14 total possible alarms, where the index
 // of the value in the array represents the alarm number.
-uint8_t weekDay[14] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};    // (0-6) (Sun - Sat)
-uint8_t hour[14]    = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};    // (0-23)
-uint8_t min[14]     = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};    // (0-59)
-bool alarm_on[14]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};    // (0-1)
+uint8_t weekDay[14];    // (0-6) (Sun - Sat)
+uint8_t hour[14];       // (0-23)
+uint8_t min[14];        // (0-59)
+bool alarm_on[14];      // (0-1)
 
-int melody[] = {1000,1500,2000,2500,3000};  // An array of frequencies
+int melody[] = {1000,1500,2000,2500,3000,2000,1000};  // An array of frequencies
 
 
 /*
@@ -58,8 +47,7 @@ int melody[] = {1000,1500,2000,2500,3000};  // An array of frequencies
 void setup(void) {
   Serial.begin(9600);
   Serial.println("Setup Start");
-  // Parameters: (cols, rows)
-  lcd.begin(16,2);
+  lcd.begin(16,2);                // Initializes LCD
   lcd.clear();
   lcd.setCursor(0, 0);
   pinMode(stClk, OUTPUT);
@@ -73,22 +61,29 @@ void setup(void) {
   digitalWrite(stClk, HIGH);  // Ends communications with register
   Wire.begin();
   RTC.begin();
+
   if (! RTC.isrunning()) {
     Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    RTC.adjust(DateTime(__DATE__, __TIME__));
   }
+  RTC.adjust(DateTime(__DATE__, __TIME__));
   Serial.println("Setup END");
+
+  /////////////////////////////////////////////////////
+  //Test Times
+  set_alarm(0, 1, 0, 0);
+  set_DateTime(0, 12, 24, 23, 59, 57);
+  //////////////////////////////////////////////////////
 }
 
+
 /*
-  Method to check if current time is the same as an alarm time that is turned ON
+  Method to check if current time is the same as an alarm time that is also turned ON
 */
 bool isTimeAlarm() {
   DateTime now = RTC.now(); 
   for (int i=0; i<14; i++) {
     if (alarm_on[i] == 1) { // only check time against set alarm time if alarm is ON (1)
-     if ((weekDay[i]) == (now.dayOfWeek())) {        
+     if ((weekDay[i]) == (now.dayOfWeek())) { // check if day of week, hour, and min = an alarm time
       if (hour[i] == now.hour()) { 
         if (min[i] == now.minute()) { 
           return true;
@@ -100,16 +95,17 @@ bool isTimeAlarm() {
   return false;
 }
 
+
 /*
-  Function to print time to serial
+  Function to print date, time, and day of the week to serial
 */
 void printTime () {
-    DateTime now = RTC.now(); 
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
+    DateTime now = RTC.now();       // Gets current time
     Serial.print(now.month(), DEC);
     Serial.print('/');
     Serial.print(now.day(), DEC);
+    Serial.print('/');
+    Serial.print(now.year(), DEC);
     Serial.print(' ');
     Serial.print(now.hour(), DEC);
     Serial.print(':');
@@ -121,23 +117,73 @@ void printTime () {
     Serial.println(); 
 }
 
+
+/*
+  Function to alert user an alarm is triggered
+*/
+void notify(){
+  send_SMS();
+
+  if (LED < 256){                                       // Uses first register
+    digitalWrite(stClk, LOW);                           // Sets register to recieve data
+    shiftOut(dataPin, srClk, LSBFIRST, LED);
+    digitalWrite(stClk, HIGH);                          // Ends communications with register
+  }
+  else if(LED >= 256){                                  // Uses second register
+    digitalWrite(stClk, LOW);                           // Sets register to recieve data
+    shiftOut(dataPin, srClk, LSBFIRST, 0);
+    shiftOut(dataPin2, srClk, LSBFIRST, (LED >> 8));    // Shifts data by 8 bits to other register
+    digitalWrite(stClk, HIGH);                          // Ends communications with register
+  }
+
+  // Sounds alarm
+  for (int thisNote = 0; thisNote < (sizeof(melody) / sizeof(melody[0])) ; thisNote++){  // Loops for each note in melody[]
+      tone(13, melody[thisNote], 500);    // Plays the note at the position in melody[] for 500ms
+      delay(500);                         // Waits for 500ms for the note to finish
+    }
+}
+
+
+/*
+  Function to take user input and set time of RTC
+*/
+void set_DateTime(int newYear, int newMon, int newDay, int newHour, int newMin, int newSec) {
+  RTC.adjust(DateTime(newYear, newMon, newDay , newHour, newMin, newSec));
+}
+
+
+/*
+  Function to take user input and set time of an alarm
+*/
+void set_alarm(int alarmNum, int newWeekDay, int newHour, int newMin) {
+  alarm_on[alarmNum] = 1;  
+  weekDay[alarmNum] = newWeekDay;
+  hour[alarmNum] = newHour;
+  min[alarmNum] = newMin;
+}
+
+/*
+  Function to send the user an SMS message
+*/
+void send_SMS(){
+
+
+
+}
+
+
 /*
 	Main method that loops forever
 */
 void loop() { 
   DateTime now = RTC.now(); 
-
-  alarm_on[0] = 1;  
-  weekDay[0] = 2;
-  hour[0] = 16;
-  min[0] = 19;
-
   printTime();
 
-  if ((isTimeAlarm() == true) && (alarms_on == 1)){
-    //notify();     
+  if ((isTimeAlarm() == true) && (alarms_on == 1) && (now.second() == 0)){  // Alarm only triggers if: current time = time of an alarm; 
+                                                                            // alarms are turned on; it is the turn of a minute
+    notify();     
   } else {
-    delay(1000);
+    delay(300);
   }
   // if keypad_flag == 1
     // if key == "set alarm key"
